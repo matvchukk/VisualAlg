c75c5e5f94d118486b49fc0dde3100395b0ddc08
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-05-25 19:54:25 +0300

    Added Quick Sort

---
+++ VisualAlg/SortingAlg/Sorts/quickSort.cpp
@@ -0,0 +1,49 @@
+#include "quickSort.h"
+
+void QuickSort::sort(){
+    int right = list->getSize() - 1;
+    quicksort(0, right);
+}
+
+int QuickSort::partition(int left, int right){
+    QTimer timer;
+    timer.setInterval(1000);
+    double pivot = list->getValueAtIndex(right);
+    int i = left - 1;
+
+    for(int j = left; j < right; ++j){
+        double currentValue = list->getValueAtIndex(j);
+
+        // Highlight
+
+        if( currentValue <= pivot){
+            i++;
+            list->swapItems(i, j);
+            QTimer::singleShot(10000 * j, [this, j]() {
+                           emit callQml(list->items());
+                       });
+            timer.start();
+                        QEventLoop loop;
+                        QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+                        loop.exec();
+        }
+    }
+     list->swapItems(i+1, right);
+     QTimer::singleShot(10000 , [this]() {
+                    emit callQml(list->items());
+                });
+     timer.start();
+                 QEventLoop loop;
+                 QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+                 loop.exec();
+    return i+1;
+}
+
+void QuickSort::quicksort(int left, int right){
+    if(left < right){
+        int pivot = partition(left, right);
+
+       quicksort(left, pivot-1);
+       quicksort(pivot+1, right);
+    }
+}
---
+++ VisualAlg/SortingAlg/Sorts/quickSort.h
@@ -0,0 +1,33 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include "SortingAlg\List\list.h"
+#include <QObject>
+#include <QTimer>
+
+class QuickSort : public QObject
+{
+    Q_OBJECT
+private:
+    List *list;
+
+public:
+
+    QuickSort( List *list):
+        list(list)
+        {
+         };
+    void quicksort(int left, int right);
+    int partition(int left, int right);
+
+public slots:
+    void sort();
+
+signals:
+    void callQml(QList<int> steplist);
+
+};
+
+#endif // QUICKSORT_H
+
+
